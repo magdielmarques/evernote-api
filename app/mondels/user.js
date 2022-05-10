0e38307dc9bc485e7c49f65aab3dcb8a1dbbd563
magdielmarques <contato.magdiel@gmail.com>
2022-05-10 10:47:48 -0400

    Adding lib bcrypt and creating new middleware to password

--- app/mondels/user.js
+++ app/mondels/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 let userSchema = new mongoose.Schema({
   name: String,
@@ -7,5 +8,20 @@ let userSchema = new mongoose.Schema({
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, defaulte: Date.now}
 });
+
+userSchema.pre('save', function (next) {
+  if (this.isNew || this.isModified('password')) {
+    bcrypt.hash(this.password, 10,
+      (err, hashedPassword) => {
+        if (err)
+          next(err)
+        else {
+          this.password = hashedPassword;
+          next();
+        }
+      }
+    )
+  }
+})
 
 module.exports  = mongoose.model('User', userSchema)
