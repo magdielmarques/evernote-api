821a17b82d8574b48f8401bb623c2a1be634153f
magdielmarques <contato.magdiel@gmail.com>
2022-05-10 10:23:35 -0400

    Creating the main models and adding Schema

---
+++ app/mondels/note.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+let noteSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  author: { //relaction with another collection (table)
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    require: true
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema);
