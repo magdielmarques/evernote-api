2fcad4c993d9e8972376f799baa081d3bc80d34a
magdielmarques <contato.magdiel@gmail.com>
2022-05-11 10:13:35 -0400

    Adding cors into the project

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var cors = require('cors');
 require('./config/database')
+
 
 var usersRouter = require('./app/routes/users');
 var notesRouter = require('./app/routes/notes');
@@ -12,6 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
